fd012a4b4341c05d80e34e3b57e4a08cf7c4a5c7
juliantvsi <95386943+juliantvsi@users.noreply.github.com>
2024-01-28 13:10:23 -0800

    asd

--- src/shared-functions.js
+++ src/shared-functions.js
@@ -60,4 +60,4 @@ export const doPasswordsMatch = (password, confirmPassword) => {
 
 /* ------------------- */
 
-export const urlFetch = 'https://lunariweb.vercel.app/'
+export const urlFetch = 'lunariweb-1edecot9j-juliantvsis-projects.vercel.app'
